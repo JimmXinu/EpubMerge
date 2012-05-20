fe9f3dcf1b29f098827266129b7243fa32320639
retiefjimm <retiefjimm@gmail.com>
2012-05-20 13:27:06 -0500

    EpubSplit - don't show config, tweak to icon image.

--- epubsplit/__init__.py
+++ epubsplit/__init__.py
@@ -26,7 +26,7 @@ class EpubSplitBase(InterfaceActionBase):
     description         = 'UI plugin to split off parts of an epub into a new book.'
     supported_platforms = ['windows', 'osx', 'linux']
     author              = 'Jim Miller'
-    version             = (0, 0, 0)
+    version             = (0, 0, 1)
     minimum_calibre_version = (0, 8, 30)
 
     #: This field defines the GUI plugin class that contains all the code
@@ -39,7 +39,7 @@ class EpubSplitBase(InterfaceActionBase):
         This method must return True to enable customization via
         Preferences->Plugins
         '''
-        return True
+        return False
 
     def config_widget(self):
         '''
