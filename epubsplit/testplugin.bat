92ae80b2d201801892edb2ce96c27ab040c07d52
retiefjimm <retiefjimm@gmail.com>
2013-11-18 08:49:57 -0600

    Save some minor changes.

--- epubsplit/testplugin.bat
+++ epubsplit/testplugin.bat
@@ -3,6 +3,6 @@ c:\Python27\python.exe makeplugin.py
 
 cp EpubSplit.zip "C:\Users\retief\AppData\Roaming\calibre\plugins\EpubSplit.zip"
 
-set CALIBRE_OVERRIDE_LANG=zz
+set CALIBRE_OVERRIDE_LANG=
 
 calibre-debug -g
