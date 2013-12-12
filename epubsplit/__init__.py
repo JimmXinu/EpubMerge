9def8bf2c2fbe3214267d6306046e79dcfd2c755
retiefjimm <retiefjimm@gmail.com>
2013-12-12 14:54:20 -0600

    Add Spanish, fix some French typoes, bump version.

--- epubsplit/__init__.py
+++ epubsplit/__init__.py
@@ -32,7 +32,7 @@ class EpubSplitBase(InterfaceActionBase):
     description         = _('UI plugin to split off parts of an epub into a new book.')
     supported_platforms = ['windows', 'osx', 'linux']
     author              = 'Jim Miller'
-    version             = (1, 3, 1)
+    version             = (1, 3, 2)
     minimum_calibre_version = (0, 9, 29)
 
     #: This field defines the GUI plugin class that contains all the code
