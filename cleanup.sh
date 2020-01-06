4144d64751b9429173fed172a36e41f93cba409b
Jim Miller <retiefjimm@gmail.com>
2020-01-06 16:53:59 -0600

    Update clean script

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
-
-/bin/find -name '*.epub'  -print0 |xargs -0 rm
-/bin/find -name '*.back'  -print0 |xargs -0 rm
-/bin/find -name '*.bak'  -print0 |xargs -0 rm
-/bin/find -name '*.pyc'  -print0 |xargs -0 rm
-/bin/find -name '*~' -print0 | xargs -0 rm
+find -name '__pycache__'  -print0 |xargs -0 rm -r
+find -name '*.epub'  -print0 |xargs -0 rm
+find -name '*.back'  -print0 |xargs -0 rm
+find -name '*.bak'  -print0 |xargs -0 rm
+find -name '*.pyc'  -print0 |xargs -0 rm
+find -name '*~' -print0 | xargs -0 rm
