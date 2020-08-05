2a147ca7972d62949a60a1e992718810b10d033c
Jim Miller <retiefjimm@gmail.com>
2020-08-04 19:16:30 -0500

    Bump min Calibre version.

--- __init__.py
+++ __init__.py
@@ -49,7 +49,7 @@ class EpubMergeBase(InterfaceActionBase):
     supported_platforms = ['windows', 'osx', 'linux']
     author              = 'Jim Miller'
     version             = (2, 9, 0)
-    minimum_calibre_version = (2, 85, 1)
+    minimum_calibre_version = (3, 48, 0)
 
     #: This field defines the GUI plugin class that contains all the code
     #: that actually does something. Its format is module_path:class_name
