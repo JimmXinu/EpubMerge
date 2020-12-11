7000ec089fbb35f065ae3b0cb3a6ffd459de7c18
Jim Miller <retiefjimm@gmail.com>
2020-12-11 10:44:28 -0600

    Update test bat

--- testplugin.bat
+++ testplugin.bat
@@ -2,9 +2,10 @@ set PYTHONIOENCODING=UTF-8
 
 python makeplugin.py
 
+rem set CALIBRE_DEVELOP_FROM=C:\Users\retief\Desktop\nook\calibre\src
 set CALIBRE_DEVELOP_FROM=
 set CALIBRE_OVERRIDE_LANG=
+set CALIBRE_USE_DARK_PALETTE=
 
 calibre-customize -a EpubMerge.zip
-rem calibre
 calibre-debug -g
