dd812e8ccc7dc7051d4bfe0c439bf49a73c1f81b
Jim Miller <retiefjimm@gmail.com>
2022-01-11 11:05:21 -0600

    Update bat files.

--- makemessages.bat
+++ makemessages.bat
@@ -1,2 +1,2 @@
-python C:\Python38\Tools\i18n\pygettext.py  epubmerge_plugin.py config.py __init__.py dialogs.py
+python C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python39\Tools\i18n\pygettext.py  epubmerge_plugin.py config.py __init__.py dialogs.py
 mv messages.pot translations
--- testplugin.bat
+++ testplugin.bat
@@ -2,10 +2,12 @@ set PYTHONIOENCODING=UTF-8
 
 python makeplugin.py
 
-rem set CALIBRE_DEVELOP_FROM=C:\Users\retief\Desktop\nook\calibre\src
+rem set CALIBRE_DEVELOP_FROM=C:\Users\%USERNAME%\Desktop\nook\calibre\src
+set CALIBRE_CONFIG_DIRECTORY=
+set CALIBRE_LIBRARY_DIRECTORY=
 set CALIBRE_DEVELOP_FROM=
 set CALIBRE_OVERRIDE_LANG=
 set CALIBRE_USE_DARK_PALETTE=
 
-calibre-customize -a EpubMerge.zip
-calibre-debug -g
+"c:\Program Files\Calibre2\calibre-customize" -a EpubMerge.zip
+"c:\Program Files\Calibre2\calibre-debug" -g
