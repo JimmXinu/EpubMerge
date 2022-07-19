0487c15cf983b86fae9a62e73642a50cd45294a4
Jim Miller <retiefjimm@gmail.com>
2022-07-19 09:14:48 -0500

    Bump Test Version 2.13.1

--- __init__.py
+++ __init__.py
@@ -48,7 +48,7 @@ class EpubMergeBase(InterfaceActionBase):
     description         = _('UI plugin to concatenate multiple epubs into one.')
     supported_platforms = ['windows', 'osx', 'linux']
     author              = 'Jim Miller'
-    version             = (2, 13, 0)
+    version             = (2, 13, 1)
     minimum_calibre_version = (3, 48, 0)
 
     #: This field defines the GUI plugin class that contains all the code
