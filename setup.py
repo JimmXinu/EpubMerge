831cdf74b15c4a053bd45fede74d101b1f5df1b9
Matheus <matheusgwdl@protonmail.com>
2024-04-10 06:44:52 +0200

    Fix

--- setup.py
+++ setup.py
@@ -102,7 +102,6 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'epubmerge=epubmerge:main',
+            'epubmerge=epubmerge.epubmerge:main',
         ],
-    },
-    scripts=['epubmerge/epubmerge.py'])
+    })
